915e469054f0fcad5fb461ee23dc538bedf34605
JustinH <justinih2003@gmail.com>
2024-08-26 20:53:21 -0700

    Allowed for clicking the squares

--- queens-clone/src/App.js
+++ queens-clone/src/App.js
@@ -1,44 +1,78 @@
+import { useState } from "react";
 import "./App.css";
 
 function generateBoard(size) {
-    let board = Array(size)
-        .fill()
-        .map(() => Array(size).fill(0));
-    let row = Math.floor(Math.random() * 7);
-    for (let i = 0; i < 7; i++) {
-        while (board[row][i] === "queen" || board[row][i] === "empty") {
-            row = Math.floor(Math.random() * 7);
+    let board = [];
+    let isDone = false;
+    while (!isDone) {
+        board = Array(size)
+            .fill()
+            .map(() => Array(size).fill(0));
+        let row = Math.floor(Math.random() * size);
+        for (let i = 0; i < size - 1; i++) {
+            while (board[row][i] === "queen" || board[row][i] === "empty") {
+                row = Math.floor(Math.random() * size);
+            }
+            board[row][i] = "queen";
+            if (row - 1 >= 0 && i - 1 >= 0) board[row - 1][i - 1] = "empty";
+            if (row - 1 >= 0 && i + 1 < size) board[row - 1][i + 1] = "empty";
+            if (row + 1 < size && i - 1 >= 0) board[row + 1][i - 1] = "empty";
+            if (row + 1 < size && i + 1 < size) board[row + 1][i + 1] = "empty";
+            for (let j = 0; j < size; j++) {
+                if (j !== row) board[j][i] = "empty";
+                if (j !== i) board[row][j] = "empty";
+            }
+            row = Math.floor(Math.random() * size);
         }
-        board[row][i] = "queen";
-        if (row - 1 >= 0 && i - 1 >= 0) board[row - 1][i - 1] = "empty";
-        if (row - 1 >= 0 && i + 1 < size) board[row - 1][i + 1] = "empty";
-        if (row + 1 < size && i - 1 >= 0) board[row + 1][i - 1] = "empty";
-        if (row + 1 < size && i + 1 < size) board[row + 1][i + 1] = "empty";
-        for (let j = 0; j < 7; j++) {
-            if (j !== row) board[j][i] = "empty";
-            if (j !== i) board[row][j] = "empty";
+        for (let i = 0; i < size; i++) {
+            if (board[i][size - 1] !== "empty") {
+                board[i][size - 1] = "queen";
+                isDone = true;
+                console.log(board);
+                break;
+            }
         }
-        row = Math.floor(Math.random() * 7);
     }
     return board;
 }
 
+function Square(props) {
+    const [squareState, setSquareState] = useState("empty");
+
+    const handleClick = () => {
+        if (squareState === "empty") setSquareState("marked");
+        else if (squareState === "marked") setSquareState("queen");
+        else setSquareState("empty");
+        console.log("Changed to: " + squareState);
+    };
+    return (
+        <div
+            className="square"
+            style={{
+                backgroundColor: props.type === "queen" ? "red" : "lightblue",
+            }}
+            onClick={handleClick}
+        >
+            {squareState === "empty" ? (
+                ""
+            ) : squareState === "marked" ? (
+                "X"
+            ) : (
+                <i className="fa-solid fa-chess-queen"></i>
+            )}
+        </div>
+    );
+}
+
 function App() {
-    let size = 7;
+    let size = 8;
     let board = generateBoard(size);
     return (
         <div className="grid">
             {board.map((row, index) => (
                 <div key={index} className="row">
                     {row.map((item, index) => (
-                        <div key={index} className="square">
-                            {item === "queen" ? (
-                                // <img src="../public/queen.jpg" alt="queen" />
-                                <div>q</div>
-                            ) : (
-                                <div>X</div>
-                            )}
-                        </div>
+                        <Square key={index} type={item} />
                     ))}
                 </div>
             ))}
